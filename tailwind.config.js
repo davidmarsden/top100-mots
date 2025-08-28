760bc1a3179b1cdcf36d0c12e35a66071011138e
davidmarsden <dmarsd@gmail.com>
2025-08-28 22:37:33 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    // require("@tailwindcss/forms"),
-    // require("@tailwindcss/typography"),
-    // require("@tailwindcss/aspect-ratio"),
-  ],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
 };
