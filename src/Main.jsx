cb860d2ef1553eb70f6f007be0b856f4ee847db5
davidmarsden <dmarsd@gmail.com>
2025-08-28 14:03:21 +0100

    Delete src/Main.jsx

--- src/Main.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import VotingApp from "./VotingApp.jsx";
-
-const el = document.getElementById("root");
-createRoot(el).render(<VotingApp />);
