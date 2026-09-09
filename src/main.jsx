08d3c4572edd513175d6a0c51d070b09362e9374
davidmarsden <dmarsd@gmail.com>
2026-09-09 23:28:49 +0100

    Add opt-in Shared Voting V2 Awards route

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import VotingApp from "./VotingApp.jsx";
+import SharedVotingAwards from "./SharedVotingAwards.jsx";
 import Top100AwardsShell from "./Top100AwardsShell.jsx";
 import "./index.css";
 import "./top100-family.css";
 
+const params = new URLSearchParams(window.location.search);
+const useSharedVotingV2 = params.get("shared-voting-v2") === "1";
 const el = document.getElementById("root");
+
 createRoot(el).render(
   <Top100AwardsShell>
-    <VotingApp />
+    {useSharedVotingV2 ? <SharedVotingAwards /> : <VotingApp />}
   </Top100AwardsShell>
 );
